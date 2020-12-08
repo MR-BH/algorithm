361167cee30457b987327a9533f8148dc6865ee8
xuyicun <xuyicun@kuaishou.com>
2020-12-08 17:42:09 +0800

    lc:117 enhance & lc:114 AC

--- 117.populating-next-right-pointers-in-each-node-ii/117.populating-next-right-pointers-in-each-node-ii.js
+++ 117.populating-next-right-pointers-in-each-node-ii/117.populating-next-right-pointers-in-each-node-ii.js
@@ -19,8 +19,56 @@
  * @param {Node} root
  * @return {Node}
  */
+// var connect = function(root) {
+//   if (root == null) return null;
+
+//   const queue = [];
+//   queue.push(root);
+//   while (queue.length > 0) {
+//     const length = queue.length;
+//     for (let i = 0; i < length; i++) {
+//       const element = queue.shift();
+//       if (element.left != null) queue.push(element.left);
+//       if (element.right != null) queue.push(element.right);
+//       if (i < length - 1) element.next = queue[0];
+//     }
+//   }
+//   return root;
+// };
+
+let last = null, nextStart = null;
+
+const handle = (p) => {
+  if (last != null) {
+    last.next = p;
+  }
+  if (nextStart == null) {
+    nextStart = p;
+  }
+  last = p;
+}
+
 var connect = function(root) {
+  if (root == null) return null;
+  let start = root;
+  while (start != null) {
+    last = null; nextStart = null;
     
+    for (let p = start; p != null; p = p.next) {
+      if (p.left != null) {
+        handle(p.left);
+      }
+  
+      if (p.right != null) {
+        handle(p.right);
+      }
+    }
+
+    start = nextStart;
+  }
+
+  return root;
 };
+
 // @lc code=end
 
