415a0084876d09a40fb99a2a226e533743aecf18
xuyicun <xuyicun@kuaishou.com>
2020-12-24 11:41:43 +0800

    lc:652 complete & enhance

--- 652.find-duplicate-subtrees/652.find-duplicate-subtrees.js
+++ 652.find-duplicate-subtrees/652.find-duplicate-subtrees.js
@@ -18,14 +18,53 @@
  * @return {TreeNode[]}
  */
 var findDuplicateSubtrees = function(root) {
-  const trees = [];
-  return findDuplicateSubtreesImpl(root, trees);
+  const answer = [];
+  const treeMap = {};
+  collect(root, answer, treeMap);
+  return answer;
 };
 
-var findDuplicateSubtreesImpl = function(root, trees) {
-  if (root === null) return;
-  findDuplicateSubtreesImpl(root.left, trees);
-  findDuplicateSubtreesImpl(root.right, trees);
+var collect = function(root, answer, treeMap) {
+  if (root === null) return '#';
+  const serial = `${root.val},${collect(root.left, answer, treeMap)},${collect(root.right, answer, treeMap)}`;
+  if (treeMap[serial] != null) {
+    treeMap[serial]++
+  } else {
+    treeMap[serial] = 1;
+  }
+  if (treeMap[serial] === 2) {
+    answer.push(root);
+  }
+  return serial;
 }
+
+// enhance
+var findDuplicateSubtrees = function(root) {
+  let t = 1;
+  const answer = [];
+  const tree = {};
+  const count = {};
+  const lookup = function(node) {
+    if (node == null) return 0;
+    const serial = `${node.val},${lookup(node.left)},${lookup(node.right)}`;
+    const uid = tree[serial] === undefined ? t++ : tree[serial];
+    tree[serial] = uid
+    if (count[uid] != null) {
+      count[uid] = ++count[uid]
+    } else {
+      count[uid] = 1;
+    }
+    if (count[uid] === 2) {
+      answer.push(node);
+    }
+    return uid;
+  }
+  lookup(root);
+  return answer;
+};
 // @lc code=end
 
+
+// @after-stub-for-debug-begin
+module.exports = findDuplicateSubtrees;
+// @after-stub-for-debug-end
